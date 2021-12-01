0730ac9e498313007472212cd11d89c08a6b2a10
Allesio <allesio.pellicciotta@gmail.com>
2021-12-01 07:50:06 +0100

    Setup for Day 2

---
+++ src/Day02.kt
@@ -0,0 +1,13 @@
+fun main() {
+    fun part1(input: List<String>): Int = 1
+    
+    fun part2(input: List<String>): Int = 1
+    
+    val testInput = readInput("Day01_test")
+    check(part1(testInput) == 1)
+    check(part2(testInput) == 1)
+    
+    val input = readInput("Day01")
+    println(part1(input))
+    println(part2(input))
+}

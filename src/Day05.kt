d02d9a45ef95efe338fef1b9747dc1b887c99324
Allesio <allesio.pellicciotta@gmail.com>
2021-12-05 14:22:28 +0100

    Solved Day 5

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,19 +1,48 @@
+import kotlin.math.max
+import kotlin.math.min
+
 fun main() {
-    fun part1(list: List<String>): Int = 1
+    val lineRgx = """(\d+),(\d+)\s+->\s+(\d+),(\d+)""".toRegex()
     
-    fun part2(list: List<String>): Int = 1
+    fun countOverlapping(lines: List<List<Int>>, includeDiagonal: Boolean = false): Int = lines
+        .mapNotNull { (x1, y1, x2, y2) ->
+            if (x1 == x2) (min(y1, y2)..max(y1, y2)).map { y -> x1 to y }
+            else if (y1 == y2) (min(x1, x2)..max(x1, x2)).map { x -> x to y1 }
+            else if (includeDiagonal){
+                if (x1 < x2) {
+                    if (y1 < y2) (x1..x2).zip(y1..y2)
+                    else (x1..x2).zip(y1 downTo y2)
+                } else {
+                    if (y1 < y2) (x1 downTo x2).zip(y1..y2)
+                    else (x1 downTo x2).zip(y1 downTo y2)
+                }
+            }
+            else null
+        }
+        .flatten()
+        .groupingBy { it }
+        .eachCount()
+        .count { it.value >= 2 }
     
-    val testInput = readInput("Day05_test")
-    check(part1(testInput) == 1)
-    check(part2(testInput) == 1)
+    fun part1(lines: List<List<Int>>): Int = countOverlapping(lines)
     
-    val input = readInput("Day05")
+    fun part2(lines: List<List<Int>>): Int = countOverlapping(lines, true)
+    
+    val testInput = readInput("Day05_test").map { textLine ->
+        textLine.getGroups(lineRgx)!!.map(String::toInt)
+    }
+    check(part1(testInput) == 5)
+    check(part2(testInput) == 12)
+    
+    val input = readInput("Day05").map { textLine ->
+        textLine.getGroups(lineRgx)!!.map(String::toInt)
+    }
     part1(input).let {
-        check(it == 1)
+        check(it == 7468)
         println(it)
     }
     part2(input).let {
-        check(it == 1)
+        check(it == 22364)
         println(it)
     }
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -18,3 +18,7 @@ fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest
 inline fun <T> List<List<T>>.forEachCol(action: (List<T>) -> Unit): Unit = indices
     .map { idx -> map { it[idx] } }
     .forEach(action)
+
+infix fun String.getGroups(regex: Regex): List<String>? = regex.find(this)
+    ?.groupValues
+    ?.drop(1)

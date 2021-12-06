76bb328bd7ddd420f2c7ec7bad699c0ed4bcaa52
Allesio <allesio.pellicciotta@gmail.com>
2021-12-06 08:09:22 +0100

    Solved part 1

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,15 +1,35 @@
 fun main() {
-    fun part1(list: List<String>): Int = 1
+    fun part1(list: List<String>): Int {
+        var fish = list.first()
+            .split(",")
+            .map(String::toInt)
+            .toMutableList()
+    
+        var amtNewFish: Int
+        
+        repeat(80) { _ ->
+            amtNewFish = fish.count { it == 0 }
+            
+            fish = fish.map { if (it == 0) 6 else it - 1 }
+                .toMutableList()
+                .also { it.addAll(List(amtNewFish) { 8 }) }
+        }
+        
+        return fish.size
+    }
     
     fun part2(list: List<String>): Int = 1
     
     val testInput = readInput("Day06_test")
-    check(part1(testInput) == 1)
+    part1(testInput).let {
+        println("testPart1: $it")
+        check(it == 5934)
+    }
     check(part2(testInput) == 1)
     
     val input = readInput("Day06")
     part1(input).let {
-        //        check(it == 1)
+        check(it == 345793)
         println(it)
     }
     part2(input).let {

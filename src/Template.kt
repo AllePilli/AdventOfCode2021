ad2c53c8943c04f63a32a39d31b23ffcc52de773
Allesio <allesio.pellicciotta@gmail.com>
2021-12-07 08:13:35 +0100

    Updated template

--- src/Template.kt
+++ src/Template.kt
@@ -3,17 +3,25 @@ fun main() {
     
     fun part2(list: List<String>): Int = 1
     
-    val testInput = readInput("Day0X_test")
-    check(part1(testInput) == 1)
-    check(part2(testInput) == 1)
-    
-    val input = readInput("Day0X")
-    part1(input).let {
-//        check(it == 1)
-        println(it)
+    readInput("Day0X_test").let { testInput ->
+        part1(testInput).let {
+            println("Test Part 1: $it")
+            check(it == 1)
+        }
+        part2(testInput).let {
+            println("Test Part 2: $it")
+            check(it == 1)
+        }
     }
-    part2(input).let {
-//        check(it == 1)
-        println(it)
+    
+    readInput("Day0X").let { input ->
+        part1(input).let {
+            //check(it == 1)
+            println(it)
+        }
+        part2(input).let {
+            //check(it == 1)
+            println(it)
+        }
     }
 }

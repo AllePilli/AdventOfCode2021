1f0b421f9bb808f7d0bb808de16380ff9d4b47b2
Allesio <allesio.pellicciotta@gmail.com>
2021-12-10 07:54:17 +0100

    Solved part 1

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,5 +1,24 @@
+import java.util.*
+
 fun main() {
-    fun part1(list: List<String>): Int = 1
+    val openChars = listOf('(', '[', '{', '<')
+    val closeChars = listOf(')', ']', '}', '>')
+    val points = closeChars.zip(listOf(3L, 57L, 1197L, 25137L)).toMap()
+    
+    fun part1(list: List<String>): Long = list.sumOf { line ->
+        val stack = Stack<Char>()
+        fun Char.notCorrectClosing(): Boolean = openChars.indexOf(stack.pop()) != closeChars.indexOf(this)
+        
+        line.fold(0L) { score, c ->
+            score + when (c) {
+                in openChars -> {
+                    stack.push(c)
+                    0
+                }
+                else -> if (stack.isEmpty() || c.notCorrectClosing()) points[c]!! else 0
+            }
+        }
+    }
     
     fun part2(list: List<String>): Int = 1
     
@@ -8,17 +27,17 @@ fun main() {
     readInput("Day10_test").prepareInput().let { testInput ->
         part1(testInput).let {
             println("Test Part 1: $it")
-            check(it == 1)
+            check(it == 26397L)
         }
         part2(testInput).let {
             println("Test Part 2: $it\n")
-            check(it == 1)
+            check(it == 288957)
         }
     }
     
     readInput("Day10").prepareInput().let { input ->
         part1(input).let {
-            //check(it == 1)
+            check(it == 343863L)
             println("Output Part 1: $it")
         }
         part2(input).let {

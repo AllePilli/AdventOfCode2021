d490348c0b024db04cd6091e5474ddd5c9002ffc
Allesio <allesio.pellicciotta@gmail.com>
2021-12-10 08:25:35 +0100

    Prepare Day 11

---
+++ src/Day11.kt
@@ -0,0 +1,29 @@
+fun main() {
+    fun part1(list: List<String>): Int = 1
+    
+    fun part2(list: List<String>): Int = 1
+    
+    fun List<String>.prepareInput(): List<String> = this
+    
+    readInput("Day11_test").prepareInput().let { testInput ->
+        part1(testInput).let {
+            println("Test Part 1: $it")
+            check(it == 1)
+        }
+        part2(testInput).let {
+            println("Test Part 2: $it\n")
+            check(it == 1)
+        }
+    }
+    
+    readInput("Day11").prepareInput().let { input ->
+        part1(input).let {
+            //check(it == 1)
+            println("Output Part 1: $it")
+        }
+        part2(input).let {
+            //check(it == 1)
+            println("Output Part 2: $it")
+        }
+    }
+}

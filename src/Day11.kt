6ad4d6d28f0020e31c53c1832bb4ee7395dd549e
Allesio <allesio.pellicciotta@gmail.com>
2021-12-11 14:10:47 +0100

    Solved Day 11 part 1

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,14 +1,65 @@
 fun main() {
-    fun part1(list: List<String>): Int = 1
+    data class Octopus(private var startValue: Int, val rowIdx: Int, val colIdx: Int) {
+        val value: Int
+            get() = startValue
+        
+        var hasFlashedThisStep = false
+            private set
+        
+        val needsToFlash: Boolean
+            get() = value >= 9 && !hasFlashedThisStep
+        
+        fun step() { startValue++ }
     
-    fun part2(list: List<String>): Int = 1
+        fun afterStep() {
+            hasFlashedThisStep = false
+            startValue = if (value > 9) 0 else value
+        }
+        
+        fun flash(): Boolean = if (needsToFlash) {
+            hasFlashedThisStep = true
+            true
+        } else false
     
-    fun List<String>.prepareInput(): List<String> = this
+        override fun toString(): String = "Octopus($value)"
+    }
+    
+    fun List<List<Octopus>>.print() = map { it.map { octopus -> octopus.value } }
+        .forEach { println(it) }
+        .also { println() }
+    
+    fun part1(octopi: List<List<Octopus>>): Int {
+        var flashCnt = 0
+        
+        repeat(100) { _ ->
+            while (octopi.anyElement(Octopus::needsToFlash)) {
+                val octopiToFlash = octopi.flatMap { it.filter(Octopus::needsToFlash) }
+                flashCnt += octopiToFlash.size
+                
+                octopiToFlash.forEach { octopus ->
+                    octopus.flash()
+                    octopi.surroundingValuesOf(octopus.rowIdx, octopus.colIdx)
+                        .forEach(Octopus::step)
+                }
+            }
+            
+            octopi.forEachElement(Octopus::step)
+            octopi.forEachElement(Octopus::afterStep)
+        }
+        
+        return flashCnt
+    }
+    
+    fun part2(list: List<List<Octopus>>): Int = 1
+    
+    fun List<String>.prepareInput(): List<List<Octopus>> = mapIndexed { rowIdx, line ->
+        line.mapIndexed { colIdx, c ->  Octopus(c.digitToInt(), rowIdx, colIdx) }
+    }
     
     readInput("Day11_test").prepareInput().let { testInput ->
         part1(testInput).let {
             println("Test Part 1: $it")
-            check(it == 1)
+            check(it == 1656)
         }
         part2(testInput).let {
             println("Test Part 2: $it\n")
--- src/Utils.kt
+++ src/Utils.kt
@@ -13,6 +13,32 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
 
 /**
+ * Runs the [action] on each element in a 2D [Iterable]
+ */
+inline fun <T> Iterable<Iterable<T>>.forEachElement(action: (T) -> Unit): Unit = forEach { row ->
+    row.forEach(action)
+}
+
+/**
+ * Returns true if at least one element in this 2D [Iterable] matches the [predicate]
+ */
+inline fun <T> Iterable<Iterable<T>>.anyElement(predicate: (T) -> Boolean): Boolean = any { it.any(predicate) }
+
+/**
+ * Returns the first element matching the [predicate], returns null if no elements match the [predicate]
+ */
+inline fun <T> Iterable<Iterable<T>>.findElement(predicate: (T) -> Boolean): T? {
+    var element: T?
+    
+    for (row in this) {
+        element = row.find(predicate)
+        if (element != null) return element
+    }
+    
+    return null
+}
+
+/**
  * Iterate columns of a 2D list
  */
 inline fun <T> List<List<T>>.forEachCol(action: (List<T>) -> Unit): Unit = indices
@@ -30,9 +56,28 @@ fun adjacentPositionsOf(rowIdx: Int, colIdx: Int): List<Pair<Int, Int>> = listOf
 )
 
 /**
- * Returns the adjacent (UP, DOWN, LEFT, RIGHT) values of a value in the 2D List
+ * Returns the surrounding (UP, DOWN, LEFT, RIGHT, DIAGONAL) coordinates of a 2D point
+ */
+fun surroundingPositionsOf(rowIdx: Int, colIdx: Int): List<Pair<Int, Int>> = buildList {
+    addAll(adjacentPositionsOf(rowIdx, colIdx))
+    add(rowIdx - 1 to colIdx + 1)
+    add(rowIdx + 1 to colIdx + 1)
+    add(rowIdx + 1 to colIdx - 1)
+    add(rowIdx - 1 to colIdx - 1)
+}
+
+/**
+ * Returns the adjacent (UP, DOWN, LEFT, RIGHT) values 2D point
  */
 fun <T> List<List<T>>.adjacentValuesOf(rowIdx: Int, colIdx: Int): List<T> = adjacentPositionsOf(rowIdx, colIdx)
+    .mapNotNull { (row, col) ->
+        try { this[row][col] } catch (ignored: IndexOutOfBoundsException) { null }
+    }
+
+/**
+ * Returns the surrounding (UP, DOWN, LEFT, RIGHT, DIAGONAL) values of a 2D point
+ */
+fun <T> List<List<T>>.surroundingValuesOf(rowIdx: Int, colIdx: Int): List<T> = surroundingPositionsOf(rowIdx, colIdx)
     .mapNotNull { (row, col) ->
         try { this[row][col] } catch (ignored: IndexOutOfBoundsException) { null }
     }

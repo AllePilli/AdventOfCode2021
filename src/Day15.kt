704a7b43cdc4b927051cc254d9060df307689872
Allesio <allesio.pellicciotta@gmail.com>
2021-12-15 08:51:47 +0100

    Extracted findPath function

--- src/Day15.kt
+++ src/Day15.kt
@@ -1,24 +1,23 @@
 import kotlin.math.min
 
 fun main() {
-    fun part1(list: List<List<Int>>): Int {
-        val destination = list.size to list.first().size
-        val cache = hashMapOf<Pair<Int, Int>, Int>()
-        
-        fun findPath(x: Int, y: Int): Int = list[x][y] + if (x to y == destination) 0 else {
-            if (x == destination.first - 1) cache.computeIfAbsent(x to y + 1) { (newX, newY) ->
-                (newY until destination.second).sumOf { itY -> list[newX][itY] }
-            } else if (y == destination.second - 1) cache.computeIfAbsent(x + 1 to y) { (newX, newY) ->
-                (newX until destination.first).sumOf { itX -> list[itX][newY] }
-            } else {
-                val left = cache[x + 1 to y] ?: findPath(x + 1, y).also { cache[x + 1 to y] = it }
-                val right = cache[x to y + 1] ?: findPath(x, y + 1).also { cache[x to y + 1] = it }
-                min(left, right)
-            }
+    fun findPath(list: List<List<Int>>, x: Int, y: Int, endX: Int, endY: Int, cache: HashMap<Pair<Int, Int>, Int>): Int
+    = list[x][y] + if (x == endX && y == endY) 0 else {
+        if (x == endX - 1) cache.computeIfAbsent(x to y + 1) { (newX, newY) ->
+            (newY until endY).sumOf { itY -> list[newX][itY] }
+        } else if (y == endY - 1) cache.computeIfAbsent(x + 1 to y) { (newX, newY) ->
+            (newX until endX).sumOf { itX -> list[itX][newY] }
+        } else {
+            val left = cache[x + 1 to y]
+                ?: findPath(list, x + 1, y, endX, endY, cache).also { cache[x + 1 to y] = it }
+            val right = cache[x to y + 1]
+                ?: findPath(list, x, y + 1, endX, endY, cache).also { cache[x to y + 1] = it }
+            min(left, right)
         }
-        
-        return findPath(0, 0) - list[0][0]
     }
+    
+    fun part1(list: List<List<Int>>): Int =
+        findPath(list, 0, 0, list.size, list.first().size, hashMapOf()) - list[0][0]
     
     fun part2(list: List<List<Int>>): Int = 1
     
